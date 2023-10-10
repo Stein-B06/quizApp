f0e922ffae1c054a0119110f4c97c5fb95ec6930
Stein-B06 <Steinb06.kodehode@gmail.com>
2023-10-10 11:49:30 +0200

    added JS

---
+++ index.js
@@ -0,0 +1,47 @@
+const questions = [
+  {
+    question: "which bla bla?",
+    answers: [
+      { text: "bla", correct: false },
+      { text: "black", correct: false },
+      { text: "blur", correct: false },
+      { text: "answer", correct: true },
+    ],
+  },
+  {
+    question: "which bla bla?",
+    answers: [
+      { text: "bla", correct: false },
+      { text: "black", correct: false },
+      { text: "blur", correct: false },
+      { text: "answer", correct: true },
+    ],
+  },
+  {
+    question: "which bla bla?",
+    answers: [
+      { text: "bla", correct: false },
+      { text: "black", correct: false },
+      { text: "blur", correct: false },
+      { text: "answer", correct: true },
+    ],
+  },
+  {
+    question: "which bla bla?",
+    answers: [
+      { text: "bla", correct: false },
+      { text: "black", correct: false },
+      { text: "blur", correct: false },
+      { text: "answer", correct: true },
+    ],
+  },
+  {
+    question: "which bla bla?",
+    answers: [
+      { text: "bla", correct: false },
+      { text: "black", correct: false },
+      { text: "blur", correct: false },
+      { text: "answer", correct: true },
+    ],
+  },
+];
